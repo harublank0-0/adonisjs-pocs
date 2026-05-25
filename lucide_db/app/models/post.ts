e7898587f67b9b268b698b5c102023f56c386d75
haruBlank0-0 <harublank00@gmail.com>
2026-05-25 18:26:29 +0545

    feat: taste hooks

--- lucide_db/app/models/post.ts
+++ lucide_db/app/models/post.ts
@@ -1,4 +1,23 @@
 import { PostSchema } from '#database/schema'
+import { beforeFind, afterCreate } from '@adonisjs/lucid/orm'
+import { DatabaseQueryBuilder } from '@adonisjs/lucid/database'
 
 export default class Post extends PostSchema {
+  /**
+   * Automatically exclude sofe-deleted posts from queries
+   * This hook modifies every find query to filter deleted records.
+   */
+  @beforeFind()
+  static ignoreDeleted(query: DatabaseQueryBuilder) {
+    query.where('isDeleted', false)
+  }
+
+  /**
+   * Send notification after creating a post
+   * After hooks receive the saved model instance
+   */
+  @afterCreate()
+  static async notifyFollowers(post: Post) {
+    // send notification to followers
+  }
 }
--- lucide_db/app/models/user.ts
+++ lucide_db/app/models/user.ts
@@ -2,6 +2,7 @@ import { UserSchema } from '#database/schema'
 import hash from '@adonisjs/core/services/hash'
 import { compose } from '@adonisjs/core/helpers'
 import { withAuthFinder } from '@adonisjs/auth/mixins/lucid'
+import { beforeSave } from '@adonisjs/lucid/orm'
 
 export default class User extends compose(UserSchema, withAuthFinder(hash)) {
   get initials() {
@@ -10,5 +11,21 @@ export default class User extends compose(UserSchema, withAuthFinder(hash)) {
       return `${first.charAt(0)}${last.charAt(0)}`.toUpperCase()
     }
     return `${first.slice(0, 2)}`.toUpperCase()
+  }
+
+  /**
+   * Hash the password before saving to the database.
+   * The hook runs before both inserts and updates.
+   */
+  // there's already a method named `hashPassword`
+  @beforeSave()
+  static async hashPasswordDemo(user: User) {
+    /**
+     * Only Hash if the password was modified
+     * The $dirty object tracks which attributes changed.
+     */
+    if (user.$dirty.password) {
+      user.password = await hash.make(user.password)
+    }
   }
 }
